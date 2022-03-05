dc0ba0131b4ab9aa7921dd53f1f95228baebc683
karanjit96 <karanjit96@gmail.com>
2022-03-05 12:42:55 +0100

    edited

--- autoclick.py
+++ autoclick.py
@@ -1,11 +1,20 @@
 
 import pyautogui
-
-pyautogui.PAUSE=5 
+global pause
+pause=5
+pyautogui.PAUSE=pause 
+print('Running')
+global counter
+counter =0
 try:
     while (1):
-            pyautogui.click(duration=1)    
+        
+        counter +=1
+        pyautogui.click(duration=1)    
             
 
 except KeyboardInterrupt:
     print('Quitting')
+    print('program ran for: {x} mins'.format(x=((counter*pause)/60)))
+    
+  
