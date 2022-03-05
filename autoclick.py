20f5780c693e8e9e69d7c8e61e4dcf4149a9ec5b
karanjit96 <karanjit96@gmail.com>
2022-03-05 12:31:04 +0100

    edited

--- autoclick.py
+++ autoclick.py
@@ -1,11 +1,11 @@
 
 import pyautogui
-import time
-#finding the size of screen
-print(pyautogui.size())
 
-#this code moves the mouse at given duration
-#pyautogui.moveTo(-100,-100,duration=1)
-while(1):
-    pyautogui.click(None,None,duration=1)
-    time.sleep(60)
+pyautogui.PAUSE=5 
+try:
+    while (1):
+            pyautogui.click(duration=1)    
+            
+
+except KeyboardInterrupt:
+    print('Quitting')
